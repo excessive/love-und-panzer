8e0a7455e8e57ebe1ece27cd03575adec1949b69
Garrett Johnson <silentdragon@gmail.com>
2012-11-17 22:56:16 -0600

    Add Basic Networking Code
    Just setting up the basics for setting up server/clients and connecting
    them together.

--- client/main.lua
+++ client/main.lua
@@ -1,5 +1,9 @@
+-- vim: set noet ts=8 sw=8:
+
 require "libs.input"
 require "libs.TEsound"
+require 'libs.LUBE.LUBE'
+PORT = 4356
 
 function love.load()
 	debug = {}
@@ -97,4 +101,39 @@ end
 
 function love.quit()
 	layers:each("quit")
+end
+
+-- Server
+
+function serverRecv(data, clientId)
+
+end
+
+function serverConn(clientId)
+end
+
+function serverDisconn(clientId)
+end
+
+function startServer()
+	conn = lube.tcpServer()
+	conn.handshake = "loveTanks"
+	conn:setPing(true, 16, "lePing\n")
+	conn:listen(PORT)
+	conn.callbacks.recv = serverRecv
+	conn.callbacks.connect = serverConn
+	conn.callbacks.disconnect = serverDisconn
+end
+
+-- Client
+
+function clientRecv(data)
+end
+
+function startClient(host, port)
+	conn = lube.tcpClient()
+	conn.handshake = "loveTanks"
+	conn:setPing(true, 2, "lePing\n")
+	conn:connect(host, port, true)
+	conn.callbacks.recv = clientRecv
 end
